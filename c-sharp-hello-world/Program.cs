e3440efd7b87088321af3fd8463dfab49c7325a3
Wilson Mun <wilsonmungit@gmail.com>
2021-02-06 19:13:28 +1100

    Initial commit

---
+++ c-sharp-hello-world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
